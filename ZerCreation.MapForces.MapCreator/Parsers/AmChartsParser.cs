c92944603b88fc58d0a3315e4300d0432124ce8e
Kacper Klukowski <mail@zercreation.com>
2019-04-25 22:27:04 +0200

    Parsing AmCharts maps - not used not tested

--- ZerCreation.MapForces.MapCreator/Parsers/AmChartsParser.cs
+++ ZerCreation.MapForces.MapCreator/Parsers/AmChartsParser.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using ZerCreation.MapForces.MapCreator.Models;
 
@@ -26,16 +27,65 @@ namespace ZerCreation.MapForces.MapCreator.Parsers
                 decimal longitude = decimal.Parse(Convert.ToString(item["longitude"]));
                 decimal latitude = decimal.Parse(Convert.ToString(item["latitude"]));
                 points.Add(new Tuple<decimal, decimal>(longitude, latitude));
+
             }
+
+            Tuple<decimal, decimal>[] transformedPoints = this.CalculateToIntegerBased(points);
+
+            IEnumerable<Point> mapPoints = transformedPoints
+                .Select(tPoint => new Point
+                {
+                    X = (int)tPoint.Item1,
+                    Y = (int)tPoint.Item2
+                });
 
             return new Map
             {
-                Points = new List<Point>
-                {
-                    new Point { X = 5, Y = 10 },
-                    new Point { X = 40, Y = 90 }
-                }
+                Points = transformedPoints.Select(tPoint => 
+                    new Point
+                    {
+                        X = (int)tPoint.Item1,
+                        Y = (int)tPoint.Item2
+                    }).ToList()
             };
+        }
+
+        private Tuple<decimal, decimal>[] CalculateToIntegerBased(List<Tuple<decimal, decimal>> amPoints)
+        {
+            Tuple<decimal, decimal>[] sortedPointsByY = amPoints.OrderBy(point => point.Item2).ToArray();
+            decimal prevY = sortedPointsByY.First().Item2;
+            int pointYValue = 0;
+
+            for (int i = 0; i < sortedPointsByY.Length; i++)
+            {
+                decimal diff = sortedPointsByY[i].Item2 - prevY;
+                if (diff > 0)
+                {
+                    pointYValue++;
+                }
+
+                prevY = sortedPointsByY[i].Item2;
+                sortedPointsByY[i] = new Tuple<decimal, decimal>(sortedPointsByY[i].Item1, pointYValue);
+            }
+
+
+            Tuple<decimal, decimal>[] sortedPointsByX = sortedPointsByY.OrderBy(point => point.Item1).ToArray();
+            decimal prevX = sortedPointsByX.First().Item1;
+            int pointXValue = 0;
+
+            for (int i = 0; i < sortedPointsByX.Length; i++)
+            {
+                decimal diff = sortedPointsByX[i].Item1 - prevX;
+                if (diff > 0)
+                {
+                    pointXValue++;
+                }
+
+                prevX = sortedPointsByX[i].Item1;
+                sortedPointsByX[i] = new Tuple<decimal, decimal>(pointXValue, sortedPointsByX[i].Item2);
+            }
+
+            return sortedPointsByX.OrderBy(point => point.Item2).ToArray();
         }
     }
 }
